0013536bef19b84a9894aeed950e672c866a4db5
Jeffrey Valdivia <jeffrey.valdivia@F1006-06-34133.bmcc.cuny.edu>
2015-10-26 15:23:42 -0400

    update 1.0

--- js/main.js
+++ js/main.js
@@ -5,5 +5,28 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
+    var myArray = [];
+    for (var i=0; i<1000; i++){
+//        myArray [i] = "item" + i;
+        myArray [i] = i;
+
+        myArray[i] = ellipse(i,i,10,10);
+    }
+    console.log(myArray);
+
+
+//        ["hello",3,"bye"];
+//    console.log(myArray[0]);
+//    console.log(myArray[1]);
+//    console.log(myArray[2]);
+//    console.log(myArray);
+//    console.log("myArray", myArray);
+
+    for (var i=0; i< myArray.lengh; i++) {
+        myArray[i] +=10
+    }
+
+    console.log(myArray);
+
 }//end setup
 
