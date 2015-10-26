f245fd5c9bd5ec020105e7bb2683b5990bf6ff0e
Jeffrey Valdivia <jeffrey.valdivia@F1006-06-34133.bmcc.cuny.edu>
2015-10-26 15:43:53 -0400

    update 2.0

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 //global variables
 
+//bubble constuction
+function bubble(){
+    //create x and y properties (variables)
+    this.x = random(width);
+    this.x = random(height);
+    this.r = 20; //r os fpr radius which is 1/2 the width of the circle
+    this.bubbleSize = this.r*2;//width and height are twice the radius
+    //draw the bubble
+    ellipse(this.x, this.y, this.bubbleSize, this.bubbleSize);
+}//end bubble
+
+
 function setup(){
     //create the canvas. put into a variable to associate with a element
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
     var myArray = [];
-    for (var i=0; i<1000; i++){
+    for (var i=0; i<10000; i++){
 //        myArray [i] = "item" + i;
         myArray [i] = i;
 
